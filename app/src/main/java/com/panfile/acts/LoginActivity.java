d4816a943417529a59450d930cb0a35fa0ac9294
wujl12@lenovo.com <wujl12@lenovo.com>
2020-05-14 17:44:00 +0800

    添加背景图

--- app/src/main/java/com/panfile/acts/LoginActivity.java
+++ app/src/main/java/com/panfile/acts/LoginActivity.java
@@ -4,14 +4,19 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Rect;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.panfile.R;
 import com.panfile.event.BusEvent;
 import com.panfile.event.MainThreadEvent;
@@ -38,7 +43,10 @@ public class LoginActivity extends AppCompatActivity {
     EditText et_pass;
     @BindView(R.id.et_login_token)
     EditText et_token;
+    @BindView(R.id.iv_login_bg)
+    ImageView iv_bg;
 
+    private MediaController mc;
     private ClientService clientService;
 
     @Override
@@ -70,11 +78,19 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    private Rect getScreenSize() {
+        Rect localRect = new Rect();
+        getWindow().getDecorView().getWindowVisibleDisplayFrame(localRect);
+        return localRect;
+    }
+
     private void init(){
         clientService = new ClientService();
         et_name.setText("1");
         et_pass.setText("1");
         et_token.setText("1");
+
+        Glide.with(this).load(R.mipmap.login_bg).into(iv_bg);
     }
 
     @OnClick({R.id.btn_login_login})
